70cecdfde359453dd3c840ea66572850ac521f12
jyotshnacs <70436766+jyotshnacs@users.noreply.github.com>
2022-11-10 16:53:11 +0000

    Task-3

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -9,10 +9,19 @@ import Post from '../Post';
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-    posts.map((samplePosts) => {
-      return <Post key={samplePosts.postId}/>
-    })
-  return <main id="main"></main>
+
+    
+  return <main id="main">{posts.map((object) => {
+    return <Post key={object.postId}
+      postId= {object.postId}
+      title= {object.title}
+      date= {object.date}
+      author= {object.author}
+      text= {object.text}
+      highlights = {object.highlights}
+      image= {object.image}
+    />
+  })}</main>
 }
 
 export default Main;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './index.css';
 
-function Post(props) {
+function Post() {
   
   return <article></article>;
 }
